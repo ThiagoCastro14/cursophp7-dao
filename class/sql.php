e2a727ff9c1f1b1c5cac14624bd2693a944b207f
Thiago Castro <thiagocastro.as@gmail.com>
2020-12-10 11:58:19 -0300

    classe usuário criada

--- class/sql.php
+++ class/sql.php
@@ -9,11 +9,11 @@ class Sql extends PDO{
 		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "root");
 	} 
 
-	private function setParams($statment, $parameters = array()){
+	private function setParams($statement, $parameters = array()){
 
 		foreach ($parameters as $key => $value) {
 
-			$this->setParam($key, $value);
+			$this->setParam($statement, $key, $value);
 		
 		}
 
@@ -21,9 +21,9 @@ class Sql extends PDO{
 	}
 
 
-	private function setParam($statment, $key, $value){
+	private function setParam($statement, $key, $value){
 
-		$statment->bindParam($key, $value);
+		$statement->bindParam($key, $value);
 	}
 
 	public function query($rawQuery, $params = array()){
