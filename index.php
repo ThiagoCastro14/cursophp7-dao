3df2da1050403fe2f477bb4d93613fd3cffe9f37
Thiago Castro <thiagocastro.as@gmail.com>
2020-12-14 10:52:27 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega 1 usuário
+//$root = new Usuario();
+//$root->loadById(1);
+//echo $root;
 
-$root->loadById(1);
+// Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
 
+//Carrega uma lista de usuários buscando pelo login
+$search = Usuario::search("Th");
+echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha
+
+//$usuario = new Usuario();
+//$usuario->login("Thiago", "lindo");
+
+//echo $usuario;
  ?>
